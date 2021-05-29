7412ff8dd77998be5f3e75a86998811c9b55911a
Carlo Jakobs <carlo.jakobs@fkie.fraunhofer.de>
2021-05-29 17:17:54 +0200

    make-all-versions.sh

--- ssdeep-2.14.1/make-all-versions.sh
+++ ssdeep-2.14.1/make-all-versions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# hash generation implementations:
+
 git checkout master
 ./configure --prefix=/tmp/ssdeep-original
 make
@@ -25,7 +27,7 @@ git checkout ssdeep-polynomial
 make
 make install
 
-git checkout ssdeep-2bTo4b
+git checkout 2bTo4b
 ./configure --prefix=/tmp/ssdeep-4b
 make
 make install
@@ -60,88 +62,94 @@ git checkout ssdeep-refactored-4b-polynomial
 make
 make install
 
+# hash comparison implemenations 		
+#		ssdeep-original       
+#		ssdeep-original-opt	    
+#		ssdeep-original-max	   
+#		ssdeep-original-pa	    
+#		ssdeep-original-opt-max    
+#		ssdeep-original-opt-pa     
+#		ssdeep-original-pa-max     
+#		ssdeep-original-opt-pa-max
 
-
-git checkout master
-git merge --no-commit rm-substring-opt
+git checkout rm-substring-opt
 ./configure --prefix=/tmp/ssdeep-original-opt
 make
 make install
-git merge --abort
 
-git checkout anchor-bug
-git merge --no-commit rm-substring-opt
-./configure --prefix=/tmp/ssdeep-bug-opt
+git checkout rm-max-score
+./configure --prefix=/tmp/ssdeep-original-max
 make
 make install
-git merge --abort
 
-git checkout ssdeep-halfh-removal
-git merge --no-commit rm-substring-opt
-./configure --prefix=/tmp/ssdeep-feature-opt
+git checkout rm-parray-opt
+./configure --prefix=/tmp/ssdeep-original-pa
 make
 make install
-git merge --abort
 
-git checkout ssdeep-djb2
-git merge --no-commit rm-substring-opt
-./configure --prefix=/tmp/ssdeep-djb2-opt
+git checkout rm-substring-opt-max
+./configure --prefix=/tmp/ssdeep-original-opt-max
 make
 make install
-git merge --abort
 
-git checkout ssdeep-polynomial
-git merge --no-commit rm-substring-opt
-./configure --prefix=/tmp/ssdeep-polynomial-opt
+git checkout rm-substring-opt-pa
+./configure --prefix=/tmp/ssdeep-original-opt-pa
 make
 make install
-git merge --abort
 
-git checkout ssdeep-2bTo4b
-git merge --no-commit rm-substring-opt
+git checkout rm-parray-opt-max
+./configure --prefix=/tmp/ssdeep-original-pa-max
+make
+make install
+
+git checkout rm-substring-opt-pa-max
+./configure --prefix=/tmp/ssdeep-original-opt-pa-max
+make
+make install
+
+# hash comparison 4b implemenations 		
+#		ssdeep-4b       
+#		ssdeep-4b-opt	    
+#		ssdeep-4b-max	   
+#		ssdeep-4b-pa	  
+#		ssdeep-original-opt-max    
+#		ssdeep-original-opt-pa     
+#		ssdeep-original-pa-max     
+#		ssdeep-original-opt-pa-max
+
+
+git checkout rm-substring-opt-4b
 ./configure --prefix=/tmp/ssdeep-4b-opt
 make
 make install
-git merge --abort
 
-git checkout ssdeep-refactored
-git merge --no-commit rm-substring-opt
-./configure --prefix=/tmp/ssdeep-refactored-opt
+git checkout rm-max-score-4b
+./configure --prefix=/tmp/ssdeep-4b-max
 make
 make install
-git merge --abort
 
-git checkout ssdeep-refactored-2bTo4b
-git merge --no-commit rm-substring-opt
-./configure --prefix=/tmp/ssdeep-refactored-4b-opt
+git checkout rm-parray-opt-4b
+./configure --prefix=/tmp/ssdeep-4b-pa
 make
 make install
-git merge --abort
 
-git checkout ssdeep-refactored-djb2
-git merge --no-commit rm-substring-opt
-./configure --prefix=/tmp/ssdeep-refactored-djb2-opt
+git checkout rm-substring-opt-max-4b
+./configure --prefix=/tmp/ssdeep-4b-opt-max
 make
 make install
-git merge --abort
 
-git checkout ssdeep-refactored-polynomial
-git merge --no-commit rm-substring-opt
-./configure --prefix=/tmp/ssdeep-refactored-polynomial-opt
+git checkout rm-substring-opt-pa-4b
+./configure --prefix=/tmp/ssdeep-4b-opt-pa
 make
 make install
-git merge --abort
 
-git checkout ssdeep-refactored-4b-djb2
-git merge --no-commit rm-substring-opt
-./configure --prefix=/tmp/ssdeep-refactored-4b-djb2-opt
+git checkout rm-parray-opt-max-4b
+./configure --prefix=/tmp/ssdeep-4b-pa-max
 make
 make install
-git merge --abort
 
-git checkout ssdeep-refactored-4b-polynomial
-git merge --no-commit rm-substring-opt
-./configure --prefix=/tmp/ssdeep-refactored-4b-polynomial-opt
+git checkout rm-substring-opt-pa-max-4b
+./configure --prefix=/tmp/ssdeep-4b-opt-pa-max
 make
 make install
-git merge --abort
+
